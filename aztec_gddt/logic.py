b85f1a5fccbf2bc2a2ae1b71f423b0db57f4b57d
Danilo Lessa Bernardineli <danilo.lessa@gmail.com>
2023-10-24 09:36:15 -0300

    sync

--- aztec_gddt/logic.py
+++ aztec_gddt/logic.py
@@ -2,12 +2,12 @@ from cadCAD_tools.types import Signal, VariableUpdate
 from aztec_gddt.types import AztecModelParams, AztecModelState
 
 def p_evolve_time(params: AztecModelParams, _2, _3, _4) -> Signal:
-    return {'delta_days': params['timestep_in_days']}
+    return {'delta_blocks': params['timestep_in_blocks']}
 
-def s_days_passed(_1, _2, _3,
+def s_block_time(_1, _2, _3,
                   state: AztecModelState,
                   signal: Signal) -> VariableUpdate:
-    return ('days_passed', state['days_passed'] + signal['delta_days'])
+    return ('block_time', state['block_time'] + signal['delta_blocks'])
 
-def s_delta_days(_1, _2, _3, _4, signal: Signal) -> VariableUpdate:
-    return ('delta_days', signal['delta_days'])
+def s_delta_blocks(_1, _2, _3, _4, signal: Signal) -> VariableUpdate:
+    return ('delta_blocks', signal['delta_blocks'])
