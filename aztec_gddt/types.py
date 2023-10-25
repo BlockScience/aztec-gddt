7b74bd62bc66ed5bbfa83cd313e0e0179d695882
Danilo Lessa Bernardineli <danilo.lessa@gmail.com>
2023-10-25 09:59:43 -0300

    add selection phase enum, selection process class

--- aztec_gddt/types.py
+++ aztec_gddt/types.py
@@ -1,17 +1,38 @@
 from typing import Annotated, TypedDict, Union
 from dataclasses import dataclass
+from enum import Enum, auto
+
+L1Blocks = Annotated[float, 'blocks']  # Number of L1 Blocks
+L2Blocks = Annotated[float, 'blocks'] # Number of L2 Blocks
 
 
-Blocks = Annotated[float, 'blocks']  # Number of days
+
+class SelectionPhase(Enum):
+    pre_proposal = 0
+    proposal = 1
+    block_reveal = 2
+    proving = 3
+    finalization = 4
+    finished = 5
+@dataclass
+class SelectionProcess:
+    expected_l2_block: L2Blocks
+    current_phase: SelectionPhase
+
+
 
 class AztecModelState(TypedDict):
-    block_time: Blocks
-    delta_blocks: Blocks
+    time_l1: L1Blocks
+    time_l2: L2Blocks
+    delta_l1_blocks: L1Blocks
+    active_processes: list[SelectionProcess]
 
 class AztecModelParams(TypedDict):
     label: str
-    timestep_in_blocks: Blocks
+    timestep_in_blocks: L1Blocks
 
 class AztecModelSweepParams(TypedDict):
     label: list[str]
-    timestep_in_blocks: list[Blocks]
+    timestep_in_blocks: list[L1Blocks]
+
+
