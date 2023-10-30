a636aac04dc43840a7e5e062b5edd41be70aad4d
Danilo Lessa Bernardineli <danilo.lessa@gmail.com>
2023-10-30 19:58:42 -0300

    add more types

--- aztec_gddt/types.py
+++ aztec_gddt/types.py
@@ -5,20 +5,34 @@ from enum import Enum, auto
 L1Blocks = Annotated[float, 'blocks']  # Number of L1 Blocks
 L2Blocks = Annotated[float, 'blocks'] # Number of L2 Blocks
 
+Probability = Annotated[float, 'probability']
+Tokens = Annotated[float, 'tokens'] # Amount of slashable tokens
+
 
 
 class SelectionPhase(Enum):
     pre_proposal = 0
     proposal = 1
-    block_reveal = 2
+    reveal = 2
     proving = 3
     finalization = 4
-    finished = 5
+    done = 5
+
 @dataclass
 class SelectionProcess:
     expected_l2_block: L2Blocks
     current_phase: SelectionPhase
+    duration_in_current_phase: L1Blocks
 
+
+@dataclass
+class Sequencer():
+    stake_amount: Tokens
+
+@dataclass
+class Proposal():
+    relevant_l2_block: L2Blocks
+    score: float
 
 
 class AztecModelState(TypedDict):
@@ -30,6 +44,12 @@ class AztecModelState(TypedDict):
 class AztecModelParams(TypedDict):
     label: str
     timestep_in_blocks: L1Blocks
+
+    stake_activation_period: L1Blocks
+    unstake_cooldown_period: L1Blocks
+
+    # Behavioral Parameters
+    block_reveal_probability: Probability
 
 class AztecModelSweepParams(TypedDict):
     label: list[str]
