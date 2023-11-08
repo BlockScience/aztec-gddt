30e219f1abddd20927419a35236fd812c79d287a
Danilo Lessa Bernardineli <danilo.lessa@gmail.com>
2023-11-08 10:09:25 -0300

    coworking session Co-authored-by: Jakob Hackel <jhackel@block.science> Co-authored-by: Octopus <octopus@block.science>

--- aztec_gddt/structure.py
+++ aztec_gddt/structure.py
@@ -1,7 +1,8 @@
 from aztec_gddt.logic import *
+from typing import Callable
+from copy import deepcopy
 
-
-AZTEC_MODEL_BLOCKS = [
+AZTEC_MODEL_BLOCKS: list[dict] = [
     {
         'label': 'Time Tracking',
         'ignore': False,
@@ -13,19 +14,35 @@ AZTEC_MODEL_BLOCKS = [
             'block_time': s_block_time,
             'delta_blocks': s_delta_blocks
         }
-    }, {
-       'label': 'Pre-phase: Stake / Unstake'
-       # TODO 
     },
     {
-        'label': 'Proposal Phase: Submit Proposal Transactions'
-        # TODO
+        'label': 'Onboards and/or Offboards users ?'
     },
     {
-        'label': 'Reveal Phase: Submit Reveal Transactions'
+        'label': 'Submit Proposals'
     }
+    {
+        'label': 'Advance Block Process Phases'
+    }, 
+    {
+        'label': 'Handle L2 reorgs'
+    }
+
 ]
 
 
-PREY_PREDATOR_MODEL_BLOCKS = [block for block in AZTEC_MODEL_BLOCKS
+
+blocks: list[dict] = []
+for block in [b for b in AZTEC_MODEL_BLOCKS if b.get('ignore', False) != True]:
+    _block = deepcopy(block)
+    for variable, suf in block.get('variables', {}).items():
+        if suf == add_suf:
+            _block['variables'][variable] = add_suf(variable)
+        elif suf == replace_suf:
+            _block['variables'][variable] = replace_suf(variable)
+        else:
+            pass
+    blocks.append(_block)
+
+AZTEC_MODEL_BLOCKS = [block for block in AZTEC_MODEL_BLOCKS
                               if block.get('ignore', False) is False]
