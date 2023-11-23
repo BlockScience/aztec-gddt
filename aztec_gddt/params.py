1c8b206dd35568157ac6d52570f778e66bd0b55e
Danilo Lessa Bernardineli <danilo.lessa@gmail.com>
2023-11-23 11:46:28 -0300

    autopep8 format

--- aztec_gddt/params.py
+++ aztec_gddt/params.py
@@ -1,10 +1,10 @@
 from aztec_gddt.types import *
 
-TIMESTEPS = 5 # TODO
-SAMPLES = 1 # TODO
+TIMESTEPS = 5  # TODO
+SAMPLES = 1  # TODO
 
 INITIAL_STATE = AztecModelState(block_time=0,
-                                 delta_blocks=0)
+                                delta_blocks=0)
 
-SINGLE_RUN_PARAMS = AztecModelParams(label='default', 
+SINGLE_RUN_PARAMS = AztecModelParams(label='default',
                                      timestep_in_blocks=1)
