29f0c3a0037acbc51290cb7f5033d42abac7412c
Danilo Lessa Bernardineli <danilo.lessa@gmail.com>
2023-11-23 12:51:37 -0300

    refactor so that code passes mypy

--- aztec_gddt/helper.py
+++ aztec_gddt/helper.py
@@ -26,9 +26,9 @@ def last_active_process(processes: list[Process]) -> Process:
 
 def select_processes_by_state(processes: list[Process], phase: SelectionPhase) -> list[Process]:
     # TODO: For a collection of processes, return only those that are in a specific phase.
-    pass
+    return processes
 
 
 def has_blown_phase_duration(process) -> bool:
     # TODO: Determine if a process has blown its phase duration. (Not sure what this means or how to calculate at moment.)
-    pass
+    return False
