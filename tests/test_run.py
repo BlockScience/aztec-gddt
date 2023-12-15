1d82028c03ccbe163b3116d8ac565fd1ae2be2ae
Danilo Lessa Bernardineli <danilo.lessa@gmail.com>
2023-12-15 16:54:53 -0300

    add run test

---
+++ tests/test_run.py
@@ -0,0 +1,17 @@
+from aztec_gddt.params import SINGLE_RUN_PARAMS, INITIAL_STATE
+from aztec_gddt.structure import AZTEC_MODEL_BLOCKS
+from aztec_gddt.utils import sim_run
+
+def test_default_run():
+    N_timesteps = 5
+    N_samples = 2
+    sweep_params = {k: [v] for k, v in SINGLE_RUN_PARAMS.items()}
+    sim_args = (INITIAL_STATE,
+                sweep_params,
+                AZTEC_MODEL_BLOCKS,
+                N_timesteps,
+                N_samples)
+    sim_df = sim_run(*sim_args)
+
+    expected_rows = N_samples * (1 + N_timesteps)
+    assert len(sim_df) == expected_rows
