f79846cae6b374d9cd1cf2be27f11daf28e92cf0
Danilo Lessa Bernardineli <danilo.lessa@gmail.com>
2024-03-11 21:19:14 -0300

    add selective history deletion

--- aztec_gddt/logic.py
+++ aztec_gddt/logic.py
@@ -899,9 +899,14 @@ def s_erase_history(params: AztecModelParams,
     """
     Logic for token supply.
     """
+
+
     for t, timestep_state in enumerate(history):
-        history[t] = [history[t][-1]]
+        history[t] = [{k: v for k, v in history[t][-1].items() if k != 'transactions'}]
+        #history[t] = [history[t][-1]]
     #     # for i, substep_state in enumerate(timestep_state):
     #     #     if i > 0:
     #     #         history[t]
+    # print(f"{state['timestep']}, {len(pickle.dumps(state))}, {len(pickle.dumps(history))}")
+    # dict(sorted({k: len(pickle.dumps(v)) for k, v in state.items()}.items(), key=lambda it: it[1], reverse=True))
     return ('timestep', state['timestep']) # type: ignore
