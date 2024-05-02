8f58a88f83a38beda6c943506f34aa65bdc45a65
Danilo Lessa Bernardineli <danilo.lessa@gmail.com>
2024-05-02 19:49:52 -0300

    to csv bug

--- aztec_gddt/experiment.py
+++ aztec_gddt/experiment.py
@@ -379,7 +379,7 @@ def psuu_exploratory_run(N_sweep_samples=-1,
         for file in files:
             dfs.append(pd.read_csv(file).reset_index())
         agg_df = pd.concat(dfs)
-        agg_df.to_pickle(str(Path(output_path) / f"trajectory_tensor.csv.zip"))
+        agg_df.to_csv(str(Path(output_path) / f"trajectory_tensor.csv.zip"))
         session = boto3.Session()
         s3 = session.client("s3")
         s3.upload_file(str(Path(output_path) / f"trajectory_tensor.csv.zip"),
