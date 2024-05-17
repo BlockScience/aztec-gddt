a3faa2e356c24ea0346da9b772886b157511bbe1
SeanMcOwen <seanmcowen@gmail.com>
2024-05-17 11:50:07 -0400

    Meta logic

--- aztec_gddt/helper.py
+++ aztec_gddt/helper.py
@@ -4,12 +4,14 @@ from typing import List
 
 from aztec_gddt.types import *
 
-def proposals_from_tx(transactions: dict[TxUUID, TransactionL1]) -> dict[TxUUID, Proposal]:
+
+def proposals_from_tx(
+    transactions: dict[TxUUID, TransactionL1]
+) -> dict[TxUUID, Proposal]:
     """
     Selects all proposals from the transactions state variable.
     """
-    return {k: v for k, v in transactions.items()
-            if type(v) == Proposal}
+    return {k: v for k, v in transactions.items() if type(v) == Proposal}
 
 
 #######################################
@@ -17,33 +19,33 @@ def proposals_from_tx(transactions: dict[TxUUID, TransactionL1]) -> dict[TxUUID,
 #######################################
 
 
-def select_processes_by_state(processes: list[Process], phase_to_select: SelectionPhase) -> list[Process]:
-    selected_processes = [
-        proc for proc in processes if proc.phase == phase_to_select]
+def select_processes_by_state(
+    processes: list[Process], phase_to_select: SelectionPhase
+) -> list[Process]:
+    selected_processes = [proc for proc in processes if proc.phase == phase_to_select]
     return selected_processes
 
 
 def has_blown_phase_duration(process) -> bool:
     return False
 
+
 #######################################
 ##  Censorship functions             ##
 #######################################
 
-def build_censor_series_from_data(data: pd.DataFrame, 
-                                     index_range, 
-                                     column_name: str, 
-                                     censoring_list: List[str]):
+
+def build_censor_series_from_data(
+    data: pd.DataFrame, index_range, column_name: str, censoring_list: List[str]
+):
     """
-    Given DataFrame and input range and list of censoring builders, return a time series 
-    describing censoring behavior. 
+    Given DataFrame and input range and list of censoring builders, return a time series
+    describing censoring behavior.
     """
     indexed_data = data.iloc[index_range]
     censored_series = indexed_data[column_name].apply(lambda x: x in censoring_list)
 
     return censored_series
-
-
 
 
 #######################################
@@ -54,43 +56,58 @@ def build_censor_series_from_data(data: pd.DataFrame,
 def bernoulli_trial(probability: float) -> bool:
     if probability > 1 or probability < 0:
         raise ValueError(
-            f"Probability must be be between 0 and 1, was given {probability}.")
+            f"Probability must be be between 0 and 1, was given {probability}."
+        )
 
     # TODO: refactor so that the seed is properly tracked
     rng = np.random.default_rng()
     rand_num = rng.uniform(low=0, high=1)
-    hit = (rand_num <= probability)
+    hit = rand_num <= probability
 
     return hit
 
 
 def total_phase_duration(p: AztecModelParams) -> L1Blocks:
-    return (p['phase_duration_proposal_max_blocks'] +
-            p['phase_duration_reveal_max_blocks'] +
-            p['phase_duration_commit_bond_max_blocks'] +
-            p['phase_duration_rollup_max_blocks'])
+    return (
+        p["phase_duration_proposal_max_blocks"]
+        + p["phase_duration_reveal_max_blocks"]
+        + p["phase_duration_commit_bond_max_blocks"]
+        + p["phase_duration_rollup_max_blocks"]
+    )
 
 
 def rewards_to_sequencer(p: AztecModelParams) -> Percentage:
-    return (1 - p['rewards_to_provers'] - p['rewards_to_relay'])
+    return 1 - p["rewards_to_provers"] - p["rewards_to_relay"]
 
 
-def trial_probability(n_trials: int,
-                      sample_probability: Probability = 0.99) -> Probability:
+def trial_probability(
+    n_trials: int, sample_probability: Probability = 0.99
+) -> Probability:
     """
-    Calculates the probability for an individual trial. 
+    Calculates the probability for an individual trial.
     """
     return 1 - (1 - sample_probability) ** (1 / n_trials)
-
 
 
 def check_for_censorship(params: AztecModelParams, state: AztecModelState) -> bool:
     time_l1 = state["time_l1"]
 
     # XXX: If there's no data, then assume that is uncensored.
-    block_is_uncensored = not(
-                                params["censorship_series_builder"].get(time_l1, True)
-                                or params["censorship_series_validator"].get(time_l1, True)
-                                )
-    
+    block_is_uncensored = not (
+        params["censorship_series_builder"].get(time_l1, True)
+        or params["censorship_series_validator"].get(time_l1, True)
+    )
+
     return block_is_uncensored
+
+
+def value_from_param_timeseries_suf(
+    params, state, param_key, var_value  # -> tuple[Any, Any]
+):
+    time_series = params[param_key]
+
+    if state["timestep"] < len(time_series):
+        value = time_series[state["timestep"]]
+    else:
+        value = time_series[-1]
+    return value
--- aztec_gddt/logic.py
+++ aztec_gddt/logic.py
@@ -12,138 +12,15 @@ import pickle
 
 from aztec_gddt.types import Agent
 from .logic_functions.primitive import generic_policy, replace_suf, add_suf
-
-
-def p_evolve_time(
-    params: AztecModelParams, _2, _3, state: AztecModelState
-) -> SignalTime:
-    """
-    Policy function giving the change in number of blocks.
-
-    Args:
-         params (AztecModelParams): The current parameters of the model.
-
-    Returns:
-        Signal:
-            a dictionary of variables that can be used in an update
-    """
-    return {"delta_blocks": params["timestep_in_blocks"]}
-
-
-def p_evolve_time_dynamical(
-    params: AztecModelParams, _2, _3, state: AztecModelState
-) -> SignalTime:
-    """
-    Policy function giving the change in number of blocks.
-
-    Args:
-         params (AztecModelParams): The current parameters of the model.
-
-    Returns:
-        Signal:
-            a dictionary of variables that can be used in an update
-    """
-    return {"delta_blocks": state["advance_l1_blocks"]}
-
-
-def s_block_time(
-    params: AztecModelParams, _2, _3, state: AztecModelState, signal: SignalTime
-):
-    """
-    State update function advancing block time.
-
-    Args:
-         params (AztecModelParams): The current parameters of the model.
-         state (AztecModelState): The current state of the model.
-         signal (Signal): The Signal, generated in p_evolve_time, of how many blocks to advance time.
-
-    Returns:
-
-            A two-element tuple that all state update functions must return.
-    """
-    return ("time_l1", state["time_l1"] + signal["delta_blocks"])  # type: ignore
-
-
-def s_delta_blocks(_1, _2, _3, _4, signal: SignalTime):
-    """
-    State update function for change in block number.
-
-    Args:
-        signal (Signal): The Signal, generated in p_evolve_time, of how many blocks to advance time.
-
-    Returns:
-        VariableUpdate
-    """
-    return ("delta_blocks", signal.get("delta_blocks", 0))
-
-
-def s_reset_advance(
-    params: AztecModelParams, _2, _3, state: AztecModelState, signal: SignalTime
-):
-    """
-    State update function advancing block time.
-
-    Args:
-         params (AztecModelParams): The current parameters of the model.
-         state (AztecModelState): The current state of the model.
-         signal (Signal): The Signal, generated in p_evolve_time, of how many blocks to advance time.
-
-    Returns:
-
-            A two-element tuple that all state update functions must return.
-    """
-    return ("advance_l1_blocks", 0)  # type: ignore
-
-
-def s_current_process_time(_1, _2, _3, state: AztecModelState, signal: SignalTime):
-    """
-    State update function for change in block number.
-
-    Args:
-        signal (Signal): The Signal, generated in p_evolve_time, of how many blocks to advance time.
-
-    Returns:
-        VariableUpdate
-    """
-    updated_process: Process | None = copy(state["current_process"])
-    if updated_process is not None:
-        # type: ignore
-        updated_process.duration_in_current_phase += signal.get("delta_blocks", 0)
-
-    return ("current_process", updated_process)
-
-
-def s_current_process_time_dynamical(
-    _1, _2, _3, state: AztecModelState, signal: SignalTime
-):
-    """
-    State update function for change in block number.
-
-    Args:
-        signal (Signal): The Signal, generated in p_evolve_time, of how many blocks to advance time.
-
-    Returns:
-        VariableUpdate
-    """
-    delta_blocks = signal.get("delta_blocks", 0)
-    updated_process: Process | None = copy(state["current_process"])
-    if updated_process is not None:
-        if delta_blocks > 0:
-            updated_process.current_phase_init_time += delta_blocks
-
-    return ("current_process", updated_process)
-
-
-def value_from_param_timeseries_suf(
-    params, state, param_key, var_value  # -> tuple[Any, Any]
-):
-    time_series = params[param_key]
-
-    if state["timestep"] < len(time_series):
-        value = time_series[state["timestep"]]
-    else:
-        value = time_series[-1]
-    return value
+from .logic_functions.meta import (
+    p_evolve_time,
+    p_evolve_time_dynamical,
+    s_block_time,
+    s_delta_blocks,
+    s_reset_advance,
+    s_current_process_time,
+    s_current_process_time_dynamical,
+)
 
 
 def s_gas_fee_l1(p: AztecModelParams, _2, _3, s, _5):
