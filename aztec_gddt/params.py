b385054f1fdedc005714e8b4186d3cada16df677
SeanMcOwen <seanmcowen@gmail.com>
2024-05-28 17:23:45 -0400

    Finish through single run params

--- aztec_gddt/params.py
+++ aztec_gddt/params.py
@@ -33,6 +33,87 @@ def steady_state_blob_gas_estimate(
     return steady_gas_fee_blob_time_series[state["time_l1"]]
 
 
+def build_censor_series_from_role(
+    data: pd.DataFrame,
+    role: str,
+    censor_list: list[str],
+    start_time: int,
+    num_timesteps: int = 1000,
+    start_time_is_block_no: bool = False,
+) -> dict[L1Blocks, bool]:
+
+    # XXX: this assumes that the DataFrame has a unique, non-missing measurement
+    # for each L1 time.
+    # L1 Time.
+    sorted_data = data.sort_values(by="date")
+
+    if censor_list is None:
+        censor_list = []
+
+    if start_time_is_block_no:
+        relevant_df = sorted_data.query(
+            f"(block_number >= {start_time}) & (block_number < {start_time + num_timesteps})"
+        )
+        censored_list = relevant_df[role].apply(lambda x: x in censor_list).to_list()
+
+        index_range_to_use: List[int] = [x for x in range(0, 0 + num_timesteps)]
+
+        censored_dict = dict(zip(index_range_to_use, censored_list))
+    else:
+        index_range_to_use: List[int] = [
+            x for x in range(start_time, start_time + num_timesteps)
+        ]
+
+        indexed_data: pd.DataFrame = sorted_data.iloc[index_range_to_use]
+
+        censored_list: list[bool] = (
+            indexed_data[role].apply(lambda x: x in censor_list).to_list()
+        )
+
+        censored_dict = dict(zip(index_range_to_use, censored_list))
+    return censored_dict
+
+
+def build_censor_params(
+    data: pd.DataFrame,
+    censoring_builders: List[str],
+    censoring_validators: List[str],
+    L1_BUFFER,
+    start_time: int,
+    num_timesteps: int = 1000,
+):
+
+    practical_num_timesteps = L1_BUFFER * num_timesteps
+
+    if start_time is None:
+        data_length = len(data)
+        start_time = random.randint(0, data_length - (practical_num_timesteps + 1))
+
+    # XXX: Currently doubling number of timesteps due to weird out-of-range errors on long runs.
+
+    censorship_builder_data: dict[L1Blocks, bool] = build_censor_series_from_role(
+        data=data,
+        censor_list=censoring_builders,
+        start_time=start_time,
+        num_timesteps=practical_num_timesteps,
+        role="builder",
+    )
+    censorship_validator_data: dict[L1Blocks, bool] = build_censor_series_from_role(
+        data=data,
+        censor_list=censoring_validators,
+        start_time=start_time,
+        num_timesteps=practical_num_timesteps,
+        role="validator",
+    )
+
+    censorship_info_dict = {
+        "censorship_series_builder": [censorship_builder_data],
+        "censorship_series_validator": [censorship_validator_data],
+    }
+
+    return censorship_info_dict
+
+
 def create_experiments(
     TIMESTEPS: int,
     SAMPLES: int,
@@ -49,6 +130,8 @@ def create_experiments(
     DEVIATION_STEADY_STATE_BLOB: Gwei = 5,
     L1_SHOCK_AMOUNT: Gwei = 150,
     BLOB_SHOCK_AMOUNT: Gwei = 150,
+    L1_INTER_SHOCK_AMPLITUDE: float = 100,
+    L1_INTER_SHOCK_PERIOD: float = 10,
 ):
     """Function which builds experiments and runs them
 
@@ -200,164 +283,60 @@ def create_experiments(
         + L1_SHOCK_AMOUNT
     )
 
+    num_points = (TIMESTEPS - final_time) - initial_time
+    t = np.arange(initial_time, initial_time + num_points)
 
-#############################################################
-## Begin: intermittent shock gas estimators defined        ##
-#############################################################
-
-L1_INTER_SHOCK_AMPLITUDE = 100  # Amplitude of wave
-L1_INTER_SHOCK_PERIOD = 10  # Period of wave
-num_points = (TIMESTEPS - final_time) - initial_time
-t = np.arange(initial_time, initial_time + num_points)
-
-raw_shock_signal = (
-    L1_INTER_SHOCK_AMPLITUDE * np.sin(2 * np.pi * t / L1_INTER_SHOCK_PERIOD)
-    + L1_INTER_SHOCK_AMPLITUDE
-)
-L1_INTER_SHOCK_SIGNAL = np.array([floor(max(x, 1)) for x in raw_shock_signal])
-
-
-intermit_shock_gas_fee_l1_time_series = np.zeros(TIMESTEPS)
-intermit_shock_gas_fee_blob_time_series = np.zeros(TIMESTEPS)
-
-intermit_shock_gas_fee_l1_time_series[0:initial_time] = steady_gas_fee_l1_time_series[
-    0:initial_time
-].copy()
-intermit_shock_gas_fee_l1_time_series[-final_time:] = steady_gas_fee_l1_time_series[
-    -final_time:
-].copy()
-intermit_shock_gas_fee_l1_time_series[initial_time : TIMESTEPS - final_time] = (
-    steady_gas_fee_l1_time_series[initial_time : TIMESTEPS - final_time].copy()
-    + L1_INTER_SHOCK_SIGNAL
-)
-
-
-GAS_FEE_L1_TIME_SERIES_LIST = [
-    steady_gas_fee_l1_time_series,
-    intermit_shock_gas_fee_l1_time_series,
-    single_shock_gas_fee_l1_time_series,
-]
-GAS_FEE_BLOB_TIME_SERIES_LIST = [
-    steady_gas_fee_blob_time_series,
-    intermit_shock_gas_fee_blob_time_series,
-    single_shock_gas_fee_blob_time_series,
-]
-
-
-#############################################################
-## End: intermittent shock gas estimators defined        ##
-#############################################################
-
-# HACK: Gas is 1 for all transactions
-DEFAULT_DETERMINISTIC_TX_ESTIMATOR = UserTransactionEstimators(
-    transaction_count=lambda _: 1,  # type: ignore
-    proposal_average_size=lambda _: 100,  # type: ignore
-    transaction_average_fee_per_size=lambda _: 50.5,  # type: ignore
-)
-
-
-DEFAULT_DETERMINISTIC_GAS_ESTIMATOR = L1GasEstimators(
-    proposal=lambda _: 100_000,  # type: ignore
-    commitment_bond=lambda _: 100_000,  # type: ignore
-    content_reveal=lambda _: 81_000,  # type: ignore
-    content_reveal_blob=lambda _: 500_000,  # type: ignore
-    rollup_proof=lambda _: 700_000,  # type: ignore
-)
-
-
-#################################################
-## Begin censorship time series information    ##
-################################################
-
-L1_TIME_SERIES_SIZE = L1_BUFFER * TIMESTEPS
-
-ALWAYS_TRUE_SERIES = {i: True for i in range(0, L1_TIME_SERIES_SIZE)}
-ALWAYS_FALSE_SERIES = {i: False for i in range(0, L1_TIME_SERIES_SIZE)}
-
-
-def build_censor_series_from_role(
-    data: pd.DataFrame,
-    role: str,
-    censor_list: list[str],
-    start_time: int,
-    num_timesteps: int = 1000,
-    start_time_is_block_no: bool = False,
-) -> dict[L1Blocks, bool]:
-
-    # XXX: this assumes that the DataFrame has a unique, non-missing measurement
-    # for each L1 time.
-    # L1 Time.
-    sorted_data = data.sort_values(by="date")
-
-    if censor_list is None:
-        censor_list = []
-
-    if start_time_is_block_no:
-        relevant_df = sorted_data.query(
-            f"(block_number >= {start_time}) & (block_number < {start_time + num_timesteps})"
-        )
-        censored_list = relevant_df[role].apply(lambda x: x in censor_list).to_list()
-
-        index_range_to_use: List[int] = [x for x in range(0, 0 + num_timesteps)]
-
-        censored_dict = dict(zip(index_range_to_use, censored_list))
-    else:
-        index_range_to_use: List[int] = [
-            x for x in range(start_time, start_time + num_timesteps)
-        ]
-
-        indexed_data: pd.DataFrame = sorted_data.iloc[index_range_to_use]
-
-        censored_list: list[bool] = (
-            indexed_data[role].apply(lambda x: x in censor_list).to_list()
-        )
-
-        censored_dict = dict(zip(index_range_to_use, censored_list))
-    return censored_dict
-
-
-def build_censor_params(
-    data: pd.DataFrame,
-    censoring_builders: List[str],
-    censoring_validators: List[str],
-    start_time: int,
-    num_timesteps: int = 1000,
-):
-
-    practical_num_timesteps = L1_BUFFER * num_timesteps
-
-    if start_time is None:
-        data_length = len(data)
-        start_time = random.randint(0, data_length - (practical_num_timesteps + 1))
-
-    # XXX: Currently doubling number of timesteps due to weird out-of-range errors on long runs.
-
-    censorship_builder_data: dict[L1Blocks, bool] = build_censor_series_from_role(
-        data=data,
-        censor_list=censoring_builders,
-        start_time=start_time,
-        num_timesteps=practical_num_timesteps,
-        role="builder",
+    raw_shock_signal = (
+        L1_INTER_SHOCK_AMPLITUDE * np.sin(2 * np.pi * t / L1_INTER_SHOCK_PERIOD)
+        + L1_INTER_SHOCK_AMPLITUDE
     )
-    censorship_validator_data: dict[L1Blocks, bool] = build_censor_series_from_role(
-        data=data,
-        censor_list=censoring_validators,
-        start_time=start_time,
-        num_timesteps=practical_num_timesteps,
-        role="validator",
+    L1_INTER_SHOCK_SIGNAL = np.array([floor(max(x, 1)) for x in raw_shock_signal])
+
+    intermit_shock_gas_fee_l1_time_series = np.zeros(TIMESTEPS)
+    intermit_shock_gas_fee_blob_time_series = np.zeros(TIMESTEPS)
+
+    intermit_shock_gas_fee_l1_time_series[0:initial_time] = (
+        steady_gas_fee_l1_time_series[0:initial_time].copy()
+    )
+    intermit_shock_gas_fee_l1_time_series[-final_time:] = steady_gas_fee_l1_time_series[
+        -final_time:
+    ].copy()
+    intermit_shock_gas_fee_l1_time_series[initial_time : TIMESTEPS - final_time] = (
+        steady_gas_fee_l1_time_series[initial_time : TIMESTEPS - final_time].copy()
+        + L1_INTER_SHOCK_SIGNAL
     )
 
-    censorship_info_dict = {
-        "censorship_series_builder": [censorship_builder_data],
-        "censorship_series_validator": [censorship_validator_data],
-    }
+    GAS_FEE_L1_TIME_SERIES_LIST = [
+        steady_gas_fee_l1_time_series,
+        intermit_shock_gas_fee_l1_time_series,
+        single_shock_gas_fee_l1_time_series,
+    ]
+    GAS_FEE_BLOB_TIME_SERIES_LIST = [
+        steady_gas_fee_blob_time_series,
+        intermit_shock_gas_fee_blob_time_series,
+        single_shock_gas_fee_blob_time_series,
+    ]
 
-    return censorship_info_dict
+    # HACK: Gas is 1 for all transactions
+    DEFAULT_DETERMINISTIC_TX_ESTIMATOR = UserTransactionEstimators(
+        transaction_count=lambda _: 1,  # type: ignore
+        proposal_average_size=lambda _: 100,  # type: ignore
+        transaction_average_fee_per_size=lambda _: 50.5,  # type: ignore
+    )
 
+    DEFAULT_DETERMINISTIC_GAS_ESTIMATOR = L1GasEstimators(
+        proposal=lambda _: 100_000,  # type: ignore
+        commitment_bond=lambda _: 100_000,  # type: ignore
+        content_reveal=lambda _: 81_000,  # type: ignore
+        content_reveal_blob=lambda _: 500_000,  # type: ignore
+        rollup_proof=lambda _: 700_000,  # type: ignore
+    )
 
-#################################################
-## End censorship time series information     ##
-################################################
+    # Put together series for censorship
+    L1_TIME_SERIES_SIZE = L1_BUFFER * TIMESTEPS
+
+    ALWAYS_TRUE_SERIES = {i: True for i in range(0, L1_TIME_SERIES_SIZE)}
+    ALWAYS_FALSE_SERIES = {i: False for i in range(0, L1_TIME_SERIES_SIZE)}
 
 
 SINGLE_RUN_PARAMS = AztecModelParams(
